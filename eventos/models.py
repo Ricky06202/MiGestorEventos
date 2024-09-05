b33ce7727513ff0aad9c1bce441d723329106d74
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-05 11:04:19 -0500

    creamos los modelos para el organizador y para los eventos

--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+# Modelo para Organizadores
+class Organizador(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
+# Modelo para Eventos
+class Evento(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha = models.DateField()
+    organizador = models.ForeignKey(Organizador, on_delete=models.CASCADE)
