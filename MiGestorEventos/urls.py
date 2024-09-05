53592a529531f8f674be4642481dfd9ba973a117
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-05 11:03:41 -0500

    configuramos las vistas que vamos a estar utilizando

--- MiGestorEventos/urls.py
+++ MiGestorEventos/urls.py
@@ -1,22 +1,13 @@
-"""MiGestorEventos URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
 
+# ruta para eventos para crear para eliminar y editar y tambien ruta para organizadores y para crear
 urlpatterns = [
+    path("eventos/", None),
+    path("eventos/crear/", None),
+    path("eventos/eliminar/", None),
+    path("eventos/editar/", None),
+    path("organizadores/", None),
+    path("organizadores/crear/", None),
     path("admin/", admin.site.urls),
 ]
