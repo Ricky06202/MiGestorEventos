0097eb80022b62d1827b24e4cab0d7080fd89dbd
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-06 13:14:58 -0500

    se arreglaron los errores y se hicieron migraciones

--- MiGestorEventos/urls.py
+++ MiGestorEventos/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from eventos.views import blanco
 
 # ruta para eventos para crear para eliminar y editar y tambien ruta para organizadores y para crear
 urlpatterns = [
-    path("eventos/", None),
-    path("eventos/crear/", None),
-    path("eventos/eliminar/", None),
-    path("eventos/editar/", None),
-    path("organizadores/", None),
-    path("organizadores/crear/", None),
+    path("eventos/", blanco),
+    path("eventos/crear/", blanco),
+    path("eventos/eliminar/", blanco),
+    path("eventos/editar/", blanco),
+    path("organizadores/", blanco),
+    path("organizadores/crear/", blanco),
     path("admin/", admin.site.urls),
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Evento
 
-# Create your views here.
+
+# crea una vista para los eventos pero basa en clases
+class EventosView:
+    def get(self, request):
+        eventos = Evento.objects.all()
+        return render(request, "eventos.html", {"eventos": eventos})
+def blanco(request):
+    return HttpResponse("Hola, Mundo!")
