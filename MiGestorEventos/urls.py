97e3be910df271b7fa57e585cfdd178a6d11a348
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-07 00:10:38 -0500

    configuramos las urls para que te dirijan a la pagina asociada

--- MiGestorEventos/urls.py
+++ MiGestorEventos/urls.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from django.urls import path
-from eventos.views import blanco
+from eventos.views import (
+    CrearEvento,
+    CrearOrganizador,
+    ListarEventos,
+    ListarOrganizadores,
+    blanco,
+)
 
-# ruta para eventos para crear para eliminar y editar y tambien ruta para organizadores y para crear
 urlpatterns = [
-    path("eventos/", blanco),
-    path("eventos/crear/", blanco),
+    path("eventos/", ListarEventos.as_view(), name="eventos"),
+    path("eventos/crear/", CrearEvento.as_view(), name="crearEvento"),
     path("eventos/eliminar/", blanco),
     path("eventos/editar/", blanco),
-    path("organizadores/", blanco),
-    path("organizadores/crear/", blanco),
+    path("organizadores/", ListarOrganizadores.as_view(), name="organizadores"),
+    path("organizadores/crear/", CrearOrganizador.as_view(), name="crearOrganizador"),
     path("admin/", admin.site.urls),
 ]
